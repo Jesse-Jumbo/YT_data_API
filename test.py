448d6996d5af9dfbfc4b461bca320d0fa72adcc8
Jesse <jessie97013@gmail.com>
2021-12-20 10:22:28 +0800

    finished and start again

--- test.py
+++ test.py
@@ -1,168 +1,37 @@
-import requests
-import json
-from datetime import datetime
+channel_info_list = [{'Channel URL': 'https://www.youtube.com/channel/UCXxAXdFh-cuYXU289kd-pNQ',
+                      'Total Video Count': '354000', 'Subscriber Count': '324', 'Total Views Count': '91739352'},
+                     {'Channel URL': 'https://www.youtube.com/channel/UCXxAXdFh-cuYXU289kd-pNQ',
+                      'Total Video Count': '354000', 'Subscriber Count': '324', 'Total Views Count': '91739352'}
+                     ]
+video_info_list = [{'Channel Title': '全明星運動會', 'Published At': 'UTC+8 2021-12-14 04:00:11+08:00',
+                    'Video URL': 'https://www.youtube.com/watch?v=-M9CK7BhwR0',
+                    'Title': '目標鎖定！他就是我的狀元！速度令人震驚、分隊令人震驚，錢姊一個指名，小傑的步調全部被打亂啦～',
+                    'Description': '測試賽：【跳遠】、【50公尺游泳】、【美式躲避球】\n終於要分隊了，大家能去到自己心目中的隊伍嗎？',
+                    'Like Count': '8045', 'Comment Count': '2341', 'View Count': '1052185'},
+                   {'Channel Title': '全明星運動會', 'Published At': 'UTC+8 2021-12-14 04:00:11+08:00',
+                    'Video URL': 'https://www.youtube.com/watch?v=-M9CK7BhwR0',
+                    'Title': '目標鎖定！他就是我的狀元！速度令人震驚、分隊令人震驚，錢姊一個指名，小傑的步調全部被打亂啦～',
+                    'Description': '測試賽：【跳遠】、【50公尺游泳】、【美式躲避球】\n終於要分隊了，大家能去到自己心目中的隊伍嗎？',
+                    'Like Count': '8045', 'Comment Count': '2341', 'View Count': '1052185'}
+                   ]
+# channel_values = ['https://www.youtube.com/channel/UCXxAXdFh-cuYXU289kd-pNQ', '354000', '324', '91739352']
+# video_values = ['全明星運動會', 'UTC+8 2021-12-14 04:00:11+08:00', 'https://www.youtube.com/watch?v=-M9CK7BhwR0',
+#               '目標鎖定！他就是我的狀元！速度令人震驚、分隊令人震驚，錢姊一個指名，小傑的步調全部被打亂啦～',
+#               '測試賽：【跳遠】、【50公尺游泳】、【美式躲避球】\n終於要分隊了，大家能去到自己心目中的隊伍嗎？', '8045', '2341', '1052185']
+channel_values = []
+video_values = []
+all_values = []
 
-YOUTUBE_API_KEY = "AIzaSyBNmtaOJYMTlBkeH5w5dL4VWl4MfxQIPa0"
+for i in range(len(channel_info_list)):
+    channel_values = []
+    for value in channel_info_list[i].values():
+        channel_values.append(value)
+    all_values.append(channel_values)
+print(all_values)
 
-filename = 'youtube_fever_video.json'
-
-all_fever_viedo = []
-fever_video_data = {"link": "", "video title": "", "channel name": "", "channel link": "",  "views": 0, "uploaded": "", "likes times": "", "subscribers": "", "comments": ""}
-
-video_title_link_list = []
-video_title_list = []
-video_channel_name_list = []
-video_channel_link_list = []
-video_views_list = []
-video_post_time_list = []
-video_like_times_list = []
-subscribers_list = []
-
-def main():
-    youtube_spider = YoutubeSpider(YOUTUBE_API_KEY)
-
-    data = youtube_spider.get_popular_video()
-
-    for channel_id in data['items']:
-        uploads_id = youtube_spider.get_channel_uploads_id(channel_id['snippet']['channelId'])
-
-    for id in data['items']:
-        vid_id = id['id']
-    # for channel_name in vid['items']:
-    #     video_channel_name_list.append(channel_name['snippet']['channelTitle'])
-    #
-    # for views in vid['items']:
-    #     video_views_list.append(views['statistics']['viewCount'])
-    #
-    # for like in vid['items']:
-    #     video_like_times_list.append(like['statistics']['likeCount'])
-
-        for video_id in vid_id:
-            print("----------------------")
-            video_info = youtube_spider.get_video(video_id)
-            print(video_info)
-
-            next_page_token = ''
-            while 1:
-                comments, next_page_token = youtube_spider.get_comments(video_id, page_token=next_page_token)
-                print(comments)
-                # 如果沒有下一頁留言，則跳離
-                if not next_page_token:
-                    break
-
-    with open(filename, 'w') as w_f:
-        youtube_fever_video = vid
-        w_f.write(json.dumps(youtube_fever_video, sort_keys=True, indent=4))
-
-        # print(youtube_fever_video)
-
-class YoutubeSpider():
-    def __init__(self, api_key):
-        self.base_url = "https://www.googleapis.com/youtube/v3/"
-        self.api_key = api_key
-
-    def get_html_to_json(self, path):
-        api_url = f"{self.base_url}{path}&key={self.api_key}"
-        r = requests.get(api_url)
-        if r.status_code == requests.codes.ok:
-            data = r.json()
-        else:
-            data = None
-        return data
-
-    def get_channel_uploads_id(self, channel_id, part='contentDetails'):
-        """取得頻道上傳影片清單的ID"""
-        # UC7ia-A8gma8qcdC6GDcjwsQ
-        path = f'channels?part={part}&id={channel_id}'
-        data = self.get_html_to_json(path)
-        try:
-            uploads_id = data['items'][0]['contentDetails']['relatedPlaylists']['uploads']
-        except KeyError:
-            uploads_id = None
-        return uploads_id
-
-    def get_popular_video(self, list_type='mostPopular', part='snippet%2CcontentDetails%2Cstatistics', max_results=50, regionCode='TW'):
-        path = f'videos?part={part}&chart={list_type}&maxResults={max_results}&regionCode={regionCode}'
-        data = self.get_html_to_json(path)
-        if not data:
-            return []
-        return data
-
-    def get_video(self, video_id, part='snippet,statistics'):
-        """取得影片資訊"""
-        # jyordOSr4cI
-        # part = 'contentDetails,id,liveStreamingDetails,localizations,player,recordingDetails,snippet,statistics,status,topicDetails'
-        path = f'videos?part={part}&id={video_id}'
-        data = self.get_html_to_json(path)
-        if not data:
-            return {}
-        # 以下整理並提取需要的資料
-        for data_item in data['items']:
-
-            try:
-                # 2019-09-29T04:17:05Z
-                time_ = datetime.strptime(data_item['snippet']['publishedAt'], '%Y-%m-%dT%H:%M:%SZ')
-            except ValueError:
-                # 日期格式錯誤
-                time_ = None
-
-            url_ = f"https://www.youtube.com/watch?v={data_item['id']}"
-
-            info = {
-                'id': data_item['id'],
-                'channelTitle': data_item['snippet']['channelTitle'],
-                'publishedAt': time_,
-                'video_url': url_,
-                'title': data_item['snippet']['title'],
-                'description': data_item['snippet']['description'],
-                'likeCount': data_item['statistics']['likeCount'],
-                'commentCount': data_item['statistics']['commentCount'],
-                'viewCount': data_item['statistics']['viewCount']
-            }
-            return info
-
-    def get_comments(self, video_id, page_token='', part='snippet', max_results=10):
-        """取得影片留言"""
-        # jyordOSr4cI
-        path = f'commentThreads?part={part}&videoId={video_id}&maxResults={max_results}&pageToken={page_token}'
-        data = self.get_html_to_json(path)
-        if not data:
-            return [], ''
-        # 下一頁的數值
-        next_page_token = data.get('nextPageToken', '')
-
-        # 以下整理並提取需要的資料
-        comments = []
-        for data_item in data['items']:
-            data_item = data_item['snippet']
-            top_comment = data_item['topLevelComment']
-            try:
-                # 2020-08-03T16:00:56Z
-                time_ = datetime.strptime(top_comment['snippet']['publishedAt'], '%Y-%m-%dT%H:%M:%SZ')
-            except ValueError:
-                # 日期格式錯誤
-                time_ = None
-
-            if 'authorChannelId' in top_comment['snippet']:
-                ru_id = top_comment['snippet']['authorChannelId']['value']
-            else:
-                ru_id = ''
-
-            ru_name = top_comment['snippet'].get('authorDisplayName', '')
-            if not ru_name:
-                ru_name = ''
-
-            comments.append({
-                'reply_id': top_comment['id'],
-                'ru_id': ru_id,
-                'ru_name': ru_name,
-                'reply_time': time_,
-                'reply_content': top_comment['snippet']['textOriginal'],
-                'rm_positive': int(top_comment['snippet']['likeCount']),
-                'rn_comment': int(data_item['totalReplyCount'])
-            })
-        return comments, next_page_token
-
-
-if __name__ == "__main__":
-    main()
+for i in range(len(video_info_list)):
+    video_values = []
+    for value in video_info_list[i].values():
+        video_values.append(value)
+        all_values[i].append(value)
+print(all_values)
--- yt_fever_video.py
+++ yt_fever_video.py
@@ -1,18 +1,24 @@
 import requests
 import json
+import csv
+import pandas as pd
+from pandas.io.json import json_normalize
 from pprint import pprint
-from datetime import datetime
-
+from datetime import datetime, timezone, timedelta
 
 YOUTUBE_API_KEY = "AIzaSyBNmtaOJYMTlBkeH5w5dL4VWl4MfxQIPa0"
-filename = 'youtube_fever_video.json'
 
-all_fever_viedo = {'yt fever video': [], 'Update time': "" }
+filenameJ = 'youtube_fever_video.json'
+filenameC = 'youtube_fever_video.csv'
 
-fever_video_data = {"ChannelInfo": [], "VideoInfo": []}
+all_fever_viedo = {'YT Trending Video': [], 'Update Time': "" }
+
+fever_video_data = {"Channel Info": [], "Video Info": []}
 
 channel_info_list = []
 video_info_list = []
+all_keys = []
+all_values = []
 
 def main():
     youtube_spider = YoutubeSpider(YOUTUBE_API_KEY)
@@ -22,15 +28,16 @@ def main():
 
         channel_info = youtube_spider.get_channel(youtube_channel_id)
         channel_info_list.append(channel_info)
-        print(channel_info_list)
 
     for id in youtube_spider.get_popular_video()['items']:
         video_id = id['id']
-
         video_info = youtube_spider.get_video(video_id)
         video_info_list.append(video_info)
 
-        next_page_token = ''
+        # r.writeheader()
+        # r.writerow(video_info.values())
+
+        # next_page_token = ''
         # while 1:
         #     comments, next_page_token = youtube_spider.get_comments(video_id, page_token=next_page_token)
         #     comments_info_list.append(comments)
@@ -42,16 +49,59 @@ def main():
         #         break
 
     for data in range(len(video_info_list)):
-        fever_video_data['ChannelInfo'] = channel_info_list[data]
-        fever_video_data['VideoInfo'] = video_info_list[data]
+        fever_video_data['Channel Info'] = channel_info_list[data]
+        fever_video_data['Video Info'] = video_info_list[data]
         # fever_video_data['CommentsInfo'] = comments_info_list[data]
-        all_fever_viedo['yt fever video'].append(fever_video_data.copy())
-    all_fever_viedo['Update time'] = datetime.today().strftime('%A, %B %d, %Y %H:%M:%S')
+        all_fever_viedo['YT Trending Video'].append(fever_video_data.copy())
+    all_fever_viedo['Update Time'] = "UTC+8 " + datetime.today().strftime('%Y-%m-%d %H:%M:%S')
 
-    with open(filename, 'w') as w_f:
+    with open(filenameJ, 'w') as w_f:
         youtube_fever_video = all_fever_viedo
         w_f.write(json.dumps(youtube_fever_video, sort_keys=True, indent=4))
 
+
+    for key in channel_info_list[0].keys():
+        all_keys.append(key)
+
+    for i in range(len(channel_info_list)):
+        channel_values = []
+        for value in channel_info_list[i].values():
+            channel_values.append(value)
+        all_values.append(channel_values)
+
+    for key in video_info_list[0].keys():
+        all_keys.append(key)
+
+    for i in range(len(video_info_list)):
+        video_values = []
+        for value in video_info_list[i].values():
+            video_values.append(value)
+            all_values[i].append(value)
+        all_values[i].append(all_fever_viedo['Update Time'])
+
+    with open(filenameC, 'w', newline='', encoding='UTF-8') as csvfiel:
+        r = csv.writer(csvfiel)
+        r.writerow(all_keys + ['Update Time'])
+        for i in all_values:
+            r.writerow(i)
+
+
+
+
+
+
+    #     all_fever_viedo['YT Trending Video'][i]['ChannelInfo'],
+    #                        # all_fever_viedo['YT Trending Video'][i]['ChannelInfo']['SubscriberCount'],
+    #                        # all_fever_viedo['YT Trending Video'][i]['ChannelInfo']['TotalVideoCount'],
+    #                        # all_fever_viedo['YT Trending Video'][i]['ChannelInfo']['TotalViewsCount'],
+    #                        # all_fever_viedo['YT Trending Video'][i]['VideoInfo']['ChannelTitle'],
+    #                        # all_fever_viedo['YT Trending Video'][i]['VideoInfo']['CommentCount'],
+    #                        # all_fever_viedo['YT Trending Video'][i]['VideoInfo']['LikeCount'],
+    #                        # all_fever_viedo['YT Trending Video'][i]['VideoInfo']['Description'],
+    #                        # all_fever_viedo['YT Trending Video'][i]['VideoInfo']['PublishedAt'],
+    #                        # all_fever_viedo['YT Trending Video'][i]['VideoInfo']['Video_url'],
+    #                        # all_fever_viedo['YT Trending Video'][i]['VideoInfo']['ViewCount'],
+    #                        # all_fever_viedo['YT Trending Video'][i]['VideoInfo']['Title'], all_fever_viedo['Update Time']
 
 class YoutubeSpider():
     def __init__(self, api_key):
@@ -95,10 +145,10 @@ class YoutubeSpider():
         url_ = f"https://www.youtube.com/channel/{data_item['id']}"
 
         info = {
-            'ChannelLink': url_,
-            'TotalVideoCount': subscriber,
-            'SubscriberCount': data_item['statistics']['videoCount'],
-            'TotalViewsCount': data_item['statistics']['viewCount']
+            'Channel URL': url_,
+            'Total Video Count': subscriber,
+            'Subscriber Count': data_item['statistics']['videoCount'],
+            'Total Views Count': data_item['statistics']['viewCount']
         }
         return info
 
@@ -115,22 +165,24 @@ class YoutubeSpider():
 
         try:
             # 2019-09-29T04:17:05Z
-            time_ = data_item['snippet']['publishedAt']
+            o_time = str(data_item['snippet']['publishedAt'])
+            r_time = datetime.strptime(o_time, '%Y-%m-%dT%H:%M:%SZ')
+            time = "UTC+8 " + str(r_time.replace(tzinfo=timezone(timedelta(hours=8))))
         except ValueError:
             # 日期格式錯誤
-            time_ = None
+            time = None
 
         url_ = f"https://www.youtube.com/watch?v={data_item['id']}"
 
         info = {
-            'ChannelTitle': data_item['snippet']['channelTitle'],
-            'PublishedAt': time_,
-            'Video_url': url_,
-            'Yitle': data_item['snippet']['title'],
+            'Channel Title': data_item['snippet']['channelTitle'],
+            'Published At': time,
+            'Video URL': url_,
+            'Title': data_item['snippet']['title'],
             'Description': data_item['snippet']['description'],
-            'LikeCount': data_item['statistics']['likeCount'],
-            'CommentCount': data_item['statistics']['commentCount'],
-            'ViewCount': data_item['statistics']['viewCount']
+            'Like Count': data_item['statistics']['likeCount'],
+            'Comment Count': data_item['statistics']['commentCount'],
+            'View Count': data_item['statistics']['viewCount']
         }
         return info
 
